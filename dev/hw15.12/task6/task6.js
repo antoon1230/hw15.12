ecaaf5bd0463cb854464e8211ec6f6b1006eaf0d
antoon1230 <120689363+antoon1230@users.noreply.github.com>
2022-12-16 02:19:30 +0300

    Delete dev directory

--- dev/hw15.12/task6/task6.js
+++
@@ -1,7 +0,0 @@
-let a = prompt('Введите слово');
-function reverseString(str) {
-    return str.split("").reverse().join("");
-  }
-let b = reverseString(a);
-if (a == b) (alert('Слово является палиндромом'));
-if (a != b) (alert('Слово не является палиндромом'));
