5000dcf0705cdb02b869b5cd7372a4788a9adc0e
antoon1230 <120689363+antoon1230@users.noreply.github.com>
2022-12-16 20:36:51 +0300

    Update task5.js

--- hw15.12/task5/task5.js
+++ hw15.12/task5/task5.js
@@ -1,3 +1,6 @@
+let min1 = Math.min.apply(null, array);
+let max1 = Math.max.apply(null, array);
+
 let array = [];
 let length = prompt("Определите длину массива");
 for (let i = 0; i < length; i++) {
@@ -5,9 +8,6 @@ let arrEl = prompt("Введите число");
 array.push(arrEl);
 }
 console.log(array);
-
-let min1 = Math.min.apply(null, array);
-let max1 = Math.max.apply(null, array);
 
 alert( min1);
 alert(max1);
